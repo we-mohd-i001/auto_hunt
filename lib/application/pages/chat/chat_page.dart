126bfaca31e72587a37f87016ba24c4097bf031e
Mohd Imran <mohd-i001@webreinvent.com>
2024-04-05 10:15:58 +0530

    Removed some unused imports.

--- lib/application/pages/chat/chat_page.dart
+++ lib/application/pages/chat/chat_page.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:yourtasks/services/firestore_services.dart';
 
 import '../../../constants/constants.dart';
 import '../../../controllers/chat_controller.dart';
