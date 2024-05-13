ca90603834068e740c1d806e6c4a877cf5ddfa95
Mohd Imran <mohd-i001@webreinvent.com>
2024-05-13 17:44:47 +0530

    Updated: migrated all the filters to the helpers/filters.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'app_config.dart';
 import 'firebase_options.dart';
 import 'vaahextendflutter/base/base_controller.dart';
 import 'vaahextendflutter/services/api_self_signed.dart';
+import 'vaahextendflutter/services/data_storage/network/helpers/filter.dart';
 import 'vaahextendflutter/services/data_storage/storage.dart';
 
 Future<void> main() async {
@@ -22,12 +23,10 @@ Future<void> main() async {
   final db = Database(useFirestore: false, collectionName: 'countries');
   var iVlue =
       await db.getCollection(neq: Neq(column: 'name', value: 'South Korea'));
-  var iVlueq =
-      await db.getDocument(eq: Eq(column: 'name', value: 'South Korea'));
+  var iVlueq = await db.getDocument(eq: Eq(column: 'name', value: 'Manhattan'));
   //await db.setDocument({'id': 6, 'name': 'Jamaica'});
-  // await db.updateDocument(
-  //   {'name': 'Manhattan'},
-  // );
+  // await db.updateDocument({'name': 'Dummy Country'},
+  //     neq: Neq(column: 'name', value: 'Manhattan'));
   print('~~~> $iVlue, Type: ${iVlue.runtimeType}');
   print('~~~> $iVlueq, Type: ${iVlueq.runtimeType}');
   BaseController baseController = Get.put(BaseController());
