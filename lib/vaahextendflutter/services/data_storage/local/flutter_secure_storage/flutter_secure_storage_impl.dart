4631b620537baca1d55e0f35cb47bdc69d348517
Mohd Imran <mohd-i001@webreinvent.com>
2024-05-16 15:29:36 +0530

    Updated: errors, code readability.

--- lib/vaahextendflutter/services/data_storage/local/flutter_secure_storage/flutter_secure_storage_impl.dart
+++ lib/vaahextendflutter/services/data_storage/local/flutter_secure_storage/flutter_secure_storage_impl.dart
@@ -1,9 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import '../../storage.dart';
 
 class FlutterSecureStorageImpl implements Storage {
-  final storage = const FlutterSecureStorage();
+  final _storage = const FlutterSecureStorage();
   @override
   Future<void> init() async {}
 
@@ -11,29 +13,32 @@ class FlutterSecureStorageImpl implements Storage {
   Future<void> create({dynamic key, dynamic value}) async {
     if (key == null && value is Map<String, String>) {
       for (String k in value.keys) {
-        await storage.write(key: k, value: value[k]);
+        await _storage.write(key: k, value: value[k]);
       }
     } else if (key is String && value is String) {
-      await storage.write(key: key, value: value);
+      await _storage.write(key: key, value: value);
     } else {
-      throw ArgumentError('To save a single entry key and value must be of type String',
-          'To save multiple entries only provide value as Map<Sring, String>, ');
+      throw ArgumentError(
+        'To save a single entry, the key and the value must be of type String,'
+        'To save multiple entries pass only the value as Map<Sring, String>',
+      );
     }
   }
 
   @override
   Future<dynamic> read({dynamic key}) async {
     if (key is String) {
-      String? result = await storage.read(key: key);
+      String? result = await _storage.read(key: key);
       return result;
     } else if (key is List<String>) {
-      List<dynamic> result = [];
+      Map<String, dynamic> result = {};
       for (String k in key) {
-        result.add(await storage.read(key: k));
+        final jsonValue = await _storage.read(key: k);
+        result[k] = jsonValue;
       }
       return result;
     } else if (key == null) {
-      Map<String, String> result = await storage.readAll();
+      Map<String, String> result = await _storage.readAll();
       return result;
     } else {
       throw ArgumentError('key must be of type String or List<String>');
@@ -49,13 +54,13 @@ class FlutterSecureStorageImpl implements Storage {
   @override
   void delete({dynamic key}) async {
     if (key is String) {
-      await storage.delete(key: key);
+      await _storage.delete(key: key);
     } else if (key is List<String>) {
       for (String k in key) {
-        await storage.delete(key: k);
+        await _storage.delete(key: k);
       }
     } else if (key == null) {
-      await storage.deleteAll();
+      await _storage.deleteAll();
     } else {
       throw ArgumentError(
         'key must be of type String or List<String>',
