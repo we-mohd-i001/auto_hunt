2914fdd2a219296373ab5526b0113e4b5a5074ee
Mohd Imran <mohd-i001@webreinvent.com>
2024-05-17 11:52:16 +0530

    Update: main.dart to check the new functionalities

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,27 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'app_config.dart';
 import 'firebase_options.dart';
 import 'vaahextendflutter/base/base_controller.dart';
 import 'vaahextendflutter/services/api_self_signed.dart';
-import 'vaahextendflutter/services/data_storage/network/helpers/filter.dart';
 import 'vaahextendflutter/services/data_storage/storage.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final Directory appDocDirectory = await getApplicationDocumentsDirectory();
+  Future<void> initHive() async {
+    Directory dir = appDocDirectory;
+    await Directory('${dir.path}/dir').create(recursive: true).then((Directory directory) async {
+      Hive.init(directory.path);
+    });
+  }
+
+  await initHive();
   // await Supabase.initialize(
   //   url: 'https://embgcnouywpgunriudxi.supabase.co',
   //   anonKey:
@@ -32,7 +42,7 @@ Future<void> main() async {
   // print('~~~> $iVlueq, Type: ${iVlueq.runtimeType}');
   BaseController baseController = Get.put(BaseController());
   HttpOverrides.global = SelfSignedHttps();
-
+  //await baseController.init(app: const MyTestApp(), errorApp: const MyTestApp());
   final Storage storage = Storage.createLocal(name: 'UserData');
   await storage.init();
 
@@ -56,6 +66,14 @@ class MyTestApp extends StatefulWidget {
   State<MyTestApp> createState() => _MyTestAppState();
 }
 
+// class _MyTestAppState1 extends State<MyTestApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     throw UnimplementedError();
+//   }
+// }
+
 class _MyTestAppState extends State<MyTestApp> {
   final Storage storage = Storage.createLocal(name: 'UserData');
 
@@ -66,37 +84,9 @@ class _MyTestAppState extends State<MyTestApp> {
   late UserLucky user1;
   late UserLucky user2;
   UserLucky emptyUser = UserLucky(name: 'null', age: 'null');
-  createUsingFirebase() async {
-    user1 = UserLucky(name: 'Firebase Check', age: '12');
-  }
-
-  createUsingMap() async {
-    Map<String, dynamic> map1 = <String, dynamic>{
-      'key': 'value',
-      'key2': 'value2',
-      'key3': ['value1arr', 'value2arr'],
-    };
-    await storage.create(key: 'map1', value: json.encode(map1));
-    String result = await storage.read(key: 'map1');
-    final map2 = json.decode(result);
-
-    print('~~~> ' '$map2');
-  }
-
-  updateAndGet() async {
-    print(await storage.read());
-    user1 = UserLucky(name: 'Dave List', age: '22');
-    user2 = UserLucky(name: 'User 2', age: '56');
-    print(await storage.read(key: [
-      'john',
-      'dave',
-    ]));
-    print(await storage.read(key: 'dave'));
-  }
-
-  getData() async {
-    var storedData1 = await storage.read(key: 'dave') as String;
-    user1 = UserLucky.fromJson(storedData1);
+  checkAssertion() async {
+    storage.create(value: {'key1': 'value1'});
+    print(await storage.read(key: 'key1'));
   }
 
   @override
@@ -116,54 +106,17 @@ class _MyTestAppState extends State<MyTestApp> {
               children: [
                 ElevatedButton(
                   onPressed: () async {
-                    final users = [
-                      UserLucky(name: 'List A', age: '2'),
-                      UserLucky(name: 'List B', age: '2'),
-                      UserLucky(name: 'List C', age: '2'),
-                      UserLucky(name: 'List D', age: '2'),
-                      UserLucky(name: 'List E', age: '2'),
-                      UserLucky(name: 'List F', age: '2'),
-                      UserLucky(name: 'List G', age: '2'),
-                      UserLucky(name: 'List H', age: '2')
-                    ];
-                    final friends = Friends(userLucky: users);
-                    // await updateAndGet();
-                    // storage.create(
-                    //   key: 'friends',
-                    //   value: friends.toJson(),
-                    // );
-
-                    final storedData = await storage.read(key: 'friends');
-                    print('~~~>{$storedData}');
-                    // user = UserLucky.fromJson(storedData ?? '');
-                    // setState(() {
-                    //   value = '${user.name} ${user.age}';
-                    // });
+                    checkAssertion();
                   },
                   child: const Text('Create'),
                 ),
+                ElevatedButton(onPressed: () async {}, child: const Text('Show')),
                 ElevatedButton(
-                    onPressed: () async {
-                      await createUsingMap();
-                      // await getData();
-                      // setState(() {
-                      //   value = user1.name;
-                      // });
-                    },
-                    child: const Text('Show')),
-                ElevatedButton(
-                  onPressed: () async {
-                    await updateAndGet();
-                    setState(() {
-                      updatedData = '${user1.name} ${user1.age}';
-                    });
-                  },
+                  onPressed: () async {},
                   child: const Text('Update'),
                 ),
                 ElevatedButton(
-                  onPressed: () async {
-                    print(await storage.read());
-                  },
+                  onPressed: () async {},
                   child: const Text('Read All'),
                 ),
               ],
@@ -196,7 +149,7 @@ class Friends {
   factory Friends.fromMap(Map<String, dynamic> map) {
     return Friends(
       userLucky: List<UserLucky>.from(
-        (map['userLucky'] as List<int>).map<UserLucky>(
+        (map['userLucky'] as List<dynamic>).map<UserLucky>(
           (x) => UserLucky.fromMap(x as Map<String, dynamic>),
         ),
       ),
