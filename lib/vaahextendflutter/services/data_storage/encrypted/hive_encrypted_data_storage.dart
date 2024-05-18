56b8ff693941940c9d72cd5f8294fb4473eb48b1
Mohd Imran <mohd-i001@webreinvent.com>
2024-05-18 13:43:28 +0530

    Added: encryption logic

--- lib/vaahextendflutter/services/data_storage/encrypted/hive_encrypted_data_storage.dart
+++ lib/vaahextendflutter/services/data_storage/encrypted/hive_encrypted_data_storage.dart
@@ -1,35 +1,73 @@
+import 'dart:convert';
+
+import 'package:hive/hive.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 import '../storage.dart';
 
 class HiveEncryptedStorage implements Storage {
-  String? name;
+  final String name;
+
   HiveEncryptedStorage({this.name = 'default'});
 
+  Box? _encryptedBox;
+
+  final _secureStorage = const FlutterSecureStorage();
+
   @override
-  Future<void> create({key, value}) {
-    // TODO: implement create
+  Future<void> init() async {
+    final encryptedKey = await _getEncryptionKeyFromString(name);
+    final encryptionKeyUint8List = base64Url.decode(encryptedKey!);
+    _encryptedBox =
+        await Hive.openBox(name, encryptionCipher: HiveAesCipher(encryptionKeyUint8List));
+  }
+
+  @override
+  Future<void> create({required String key, required String value}) async {
+    _encryptedBox!.put(key, value);
+    print(_encryptedBox!.get('secret'));
+  }
+
+  @override
+  Future<void> createAll({required Map<String, String> values}) {
+    // TODO: implement createAll
     throw UnimplementedError();
   }
 
   @override
-  void delete({key}) {
-    // TODO: implement delete
-  }
-
-  @override
-  Future<void> init() {
-    // TODO: implement init
-    throw UnimplementedError();
-  }
-
-  @override
-  Future read({key}) {
+  Future<String?> read({required String key}) {
     // TODO: implement read
     throw UnimplementedError();
   }
 
   @override
-  Future update({key, value}) {
-    // TODO: implement update
+  Future<Map<String, String?>> readAll({List<String>? keys}) {
+    // TODO: implement readAll
     throw UnimplementedError();
+  }
+
+  @override
+  Future<void> delete({required String key}) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> deleteAll({List<String>? keys}) {
+    // TODO: implement deleteAll
+    throw UnimplementedError();
+  }
+
+  Future<String?> _getEncryptionKeyFromString(String key) async {
+    final encryptionKeyString = await _secureStorage.read(key: key);
+    if (encryptionKeyString == null) {
+      final hiveKey = Hive.generateSecureKey();
+      await _secureStorage.write(
+        key: key,
+        value: base64UrlEncode(hiveKey),
+      );
+    }
+    final secureKey = await _secureStorage.read(key: key);
+    return secureKey;
   }
 }
