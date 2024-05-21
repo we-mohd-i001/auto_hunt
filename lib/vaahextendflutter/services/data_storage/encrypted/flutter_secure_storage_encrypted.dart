cc0385c0f8854ca5334376bc781a8b8ef1df65cb
Mohd Imran <mohd-i001@webreinvent.com>
2024-05-21 19:10:09 +0530

    Added: implementation of encrypted flutter secure storage
    Added: encrypt package 
    
    Added: encryption logic
    
    
    Added: implementation of encrypted flutter secure storage
    
    
    Updated: Improved code readability

--- lib/vaahextendflutter/services/data_storage/encrypted/flutter_secure_storage_encrypted.dart
+++ lib/vaahextendflutter/services/data_storage/encrypted/flutter_secure_storage_encrypted.dart
@@ -1,42 +1,62 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:encrypt/encrypt.dart' as encrypt;
 
 import '../storage.dart';
 
 class FlutterSecureStorageEncryptedImpl implements Storage {
-  final _storage = const FlutterSecureStorage();
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+  encrypt.Key? key;
+  encrypt.IV? iv;
+  encrypt.Encrypter? encrypter;
+
   @override
-  Future<void> init() async {}
+  Future<void> init() async {
+    key = encrypt.Key.fromUtf8('athirtytwocharacterslongstring00');
+    iv = encrypt.IV.fromLength(16);
+    encrypter = encrypt.Encrypter(encrypt.AES(key!));
+  }
 
   @override
   Future<void> create({required String key, required String value}) async {
-    await _storage.write(key: key, value: value);
+    assert(encrypter != null, 'Encrypter was not initialized.');
+    final String encryptedValue = encrypter!.encrypt(value, iv: iv).base64;
+    await _storage.write(key: key, value: encryptedValue);
   }
 
   @override
   Future<void> createAll({required Map<String, String> values}) async {
-    for (String k in values.keys) {
-      await _storage.write(key: k, value: values[k]);
+    assert(encrypter != null, 'Encrypter was not initialized.');
+    for (MapEntry<String, String> entry in values.entries) {
+      final String encryptedValue = encrypter!.encrypt(entry.value, iv: iv).base64;
+      await _storage.write(key: entry.key, value: encryptedValue);
     }
   }
 
   @override
   Future<String?> read({required String key}) async {
-    String? result = await _storage.read(key: key);
-    return result;
+    final String? encryptedValue = await _storage.read(key: key);
+    if (encryptedValue == null) return null;
+    assert(encrypter != null, 'Encrypter was not initialized.');
+    final String decryptedValue = encrypter!.decrypt64(encryptedValue, iv: iv);
+    return decryptedValue;
   }
 
   @override
   Future<Map<String, String?>> readAll({List<String>? keys}) async {
-    if (keys is List<String>) {
-      Map<String, String?> result = {};
+    assert(encrypter != null, 'Encrypter was not initialized.');
+    final Map<String, String?> result = {};
+    if (keys != null) {
       for (String k in keys) {
-        result[k] = await _storage.read(key: k);
+        final String? encryptedValue = await _storage.read(key: k);
+        result[k] = encryptedValue == null ? null : encrypter!.decrypt64(encryptedValue, iv: iv);
       }
-      return result;
     } else {
-      final Map<String, String> result = await _storage.readAll();
-      return result;
+      final Map<String, String> encryptedResult = await _storage.readAll();
+      for (MapEntry<String, String> entry in encryptedResult.entries) {
+        result[entry.key] = encrypter!.decrypt64(entry.value, iv: iv);
+      }
     }
+    return result;
   }
 
   @override
